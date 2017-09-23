7b2d8accb78856286171fb6dce92fa8e664241d4
Henrik Christensen <sensen1695@hotmail.com>
2017-09-23 23:30:44 +0200

    created a method to show all the elements in the periodic table

---
+++ element_list.py
@@ -0,0 +1,49 @@
+import json
+
+
+class ElementList:
+    def read(self):
+        with open('elements.json') as elements:
+            element = json.load(elements)
+            return element
+
+    def all(self):
+        element = self.read()
+        for ele in element:
+            print('{}: {}'.format(ele['Number'], ele['Name']))
+            print('-' * 15)
+            print('Kemisk symbol: {}'.format(ele['Symbol']))
+            print('Protoner: {}'.format(ele['Protons']))
+
+            if ele['Neutrons'] is not None:
+                print('Neutroner: {}'.format(ele['Neutrons']))
+            else:
+                print('Neutroner: ---')
+
+            print('Elektroner: {}'.format(ele['Electrons']))
+
+            if ele['Weight'] is not None:
+                print('Vægt: {}'.format(ele['Weight']))
+            else:
+                print('Vægt: ---')
+
+            if ele['NobleGas'] is True:
+                print('- Ædelgas')
+
+            if ele['IsMetal'] is True:
+                print('- Metal')
+            else:
+                print('- Ikke Metal')
+
+            if ele['IsLanthanide'] is True:
+                print('- Lanthanide')
+
+            if ele['IsActinides'] is True:
+                print('- Actinide')
+                
+            print('\n')
+
+
+if __name__ == '__main__':
+    element = ElementList()
+    element.all()
